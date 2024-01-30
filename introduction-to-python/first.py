98d041293f5919b68a48dc5e23d1273613527c35
Anthony <zyxanthony15@gmail.com>
2024-01-30 23:35:16 +0800

    Adding a scenario

--- introduction-to-python/first.py
+++ introduction-to-python/first.py
@@ -9,3 +9,18 @@ print(4 / 2)
 #Creating a variable
 savings = 100
 print(savings)
+
+#Here's a situation
+# Create the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+
+
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+
+# Add new_savings to your savings
+total_savings = new_savings + savings
+
+# Print total_savings
+print("Your total savings are: ", total_savings)
