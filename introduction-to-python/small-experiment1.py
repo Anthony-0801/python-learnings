c2d6e4deb3b400cf5a7565a934acb7f700a6cfba
Anthony <141275535+Anthony-0801@users.noreply.github.com>
2024-05-21 23:00:34 +0800

    Update small-experiment1.py

--- introduction-to-python/small-experiment1.py
+++ introduction-to-python/small-experiment1.py
@@ -4,6 +4,16 @@ address= input ("Please enter your address: ")
 print (name, age, address)
 
 
-# You're building 10 ships per month and that cost at least 20,000,000 plus with the maintenance of 1,000,000
-# Now yu sell each ship at the price of 3,000,000. Determine if you have "Profit" to be gained, "Loss", or "Break Even"
+# You're building 10 ships per month and that cost at least 20,000,000 plus with the maintenance of 1,000,000 insurance.
+# Now you sell each ship at the price of 3,000,000. Determine if you have "Profit" to be gained, "Loss", or "Broke Even"
 
+total_cost = 21000000
+sold = input()
+total_profit = int(sold) * 3000000
+
+if total_cost > total_profit:
+  print("Loss")
+elif total_cost < total_profit:
+  print("Profit")
+elif total_cost == total_profit:
+  print("Broke Even")
