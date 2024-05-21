dc88df1ebbf510b30a76c003e297e5a149b5b468
Anthony <141275535+Anthony-0801@users.noreply.github.com>
2024-05-21 22:55:31 +0800

    Update small-experiment1.py

--- introduction-to-python/small-experiment1.py
+++ introduction-to-python/small-experiment1.py
@@ -2,3 +2,8 @@ name= input ("Please enter your name: ")
 age= input ("Please enter your age: ")
 address= input ("Please enter your address: ")
 print (name, age, address)
+
+
+# You're building 10 ships per month and that cost at least 20,000,000 plus with the maintenance of 1,000,000
+# Now yu sell each ship at the price of 3,000,000. Determine if you have "Profit" to be gained, "Loss", or "Break Even"
+
